22e6d7b9d90c45291ea2b8dd3404995cf2ed6c04
KareemSalem7 <kareem.salem2003@gmail.com>
2026-07-22 16:00:32 -0400

    added endpoint for getting all ingredients

--- backend/src/main/java/com/salemapplications/IngredientController.java
+++ backend/src/main/java/com/salemapplications/IngredientController.java
@@ -1,9 +1,22 @@
 package com.salemapplications;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/ingredients")
 public class IngredientController {
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public List<Ingredient> getIngredients() {
+        return ingredientRepository.findAll();
+    }
 }
